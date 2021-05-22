6211677a5e6a891089063b433debaee7beef7032
Alex Adkins <alex.t.adkins@gmail.com>
2021-05-21 22:45:04 -0400

    initial code commit

--- src/com/skilldistillery/app/FoodTruck.java
+++ src/com/skilldistillery/app/FoodTruck.java
@@ -1,5 +1,60 @@
 package com.skilldistillery.app;
 
 public class FoodTruck {
+	private static int ID;
+	private int uniqueID;
+	private String name;
+	private String foodType;
+	private double rating;
+
+	public FoodTruck(String name, String foodType, double rating) {
+		this.name = name;
+		this.foodType = foodType;
+		this.rating = rating;
+		this.uniqueID = ID +1;
+		ID++;
+	}
+	
+	public static int getID() {
+		return ID;
+	}
+
+	public static void setID(int iD) {
+		ID = iD;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getFoodType() {
+		return foodType;
+	}
+
+	public void setFoodType(String foodType) {
+		this.foodType = foodType;
+	}
+
+	public double getRating() {
+		return rating;
+	}
+
+	public void setRating(double rating) {
+		this.rating = rating;
+	}
+	
+	public String toString() {
+		String output = "Name: " + this.name + "\nFood Type: " + foodType + "\nRating: "
+						+ rating + "\nID: " + uniqueID;
+		return output;
+	}
+	
+	public void listFoodTrucks() {
+		
+	}
 
 }
--- src/com/skilldistillery/app/FoodTruckApp.java
+++ src/com/skilldistillery/app/FoodTruckApp.java
@@ -1,10 +1,123 @@
 package com.skilldistillery.app;
 
+import java.util.Scanner;
+
 public class FoodTruckApp {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		Scanner input = new Scanner(System.in);
+		FoodTruckApp fta = new FoodTruckApp();
+		FoodTruck[] foodTruckArr = new FoodTruck[50];
+		fta.getFoodTrucks(input, fta, foodTruckArr);
+	}
+	
+	public void getFoodTrucks(Scanner input, FoodTruckApp fta, FoodTruck[] foodTruckArr) {
+		int i = 0;
+		boolean isRunning = true;
+		while (isRunning) {
+			String name = getFoodTruckName(input, fta, foodTruckArr);
+			if(name.equalsIgnoreCase("quit")) {
+				isRunning = false;
+				fta.menu(input, fta, foodTruckArr);
+				break;
+			}
+			String foodType = fta.getFoodType(input, fta, foodTruckArr);
+			double rating = fta.getRating(input, fta, foodTruckArr);
+			foodTruckArr[i] = new FoodTruck(name, foodType, rating);
+			i++;
+		}
+		fta.menu(input, fta, foodTruckArr);
+	}
+	
+	public String getFoodTruckName(Scanner input, FoodTruckApp fta, FoodTruck[] foodTruckArr) {
+		System.out.println("Enter name of food truck: ");
+		String name = input.nextLine();
+		return name;
+	}
+	
+	public String getFoodType(Scanner input, FoodTruckApp fta, FoodTruck[] foodTruckArr) {
+		System.out.println("What kind of food do they sell?");
+		String foodType = input.nextLine();
+		return foodType;
+	}
+	
+	public double getRating(Scanner input, FoodTruckApp fta, FoodTruck[] foodTruckArr) {
+		System.out.println("What rating do you give them?");
+		double rating = input.nextDouble();
+		input.nextLine();
+		return rating;
+	}
+	
+	public void menu(Scanner input, FoodTruckApp fta, FoodTruck[] foodTruckArr) {
+		boolean menu = true;
+		while (menu) {
+			System.out.println("++++++++Choose an option++++++++");
+			System.out.println("1. List all food trucks");
+			System.out.println("2. See the average rating");
+			System.out.println("3. Display the highest-rated truck");
+			System.out.println("4. QUIT");
+			System.out.println("++++++++++++++++++++++++++++++++");
+			int choice = input.nextInt();
+			switch (choice) {
+			case 1:
+				fta.listAllFoodTrucks(foodTruckArr);
+				break;
+			case 2:
+				fta.getAverageRating(foodTruckArr);
+				break;
+			case 3:
+				fta.getHighestRatedTruck(foodTruckArr);
+				break;
+			case 4:
+				menu = false;
+				System.exit(0);
+				break;
+			}
+		}
 	}
 
+	public void listAllFoodTrucks(FoodTruck[] foodTruckArr) {
+		for (int i = 0; i < foodTruckArr.length; i++) {
+			if (foodTruckArr[i] == null) {
+				continue;
+			}
+			System.out.println("------------------------");
+			System.out.println(foodTruckArr[i].toString());
+			System.out.println("------------------------");
+		}
+	}
+
+	public void getAverageRating(FoodTruck[] foodTruckArr) {
+		double sum = 0;
+		double average = 0;
+		int j = 0;
+		for (int i = 0; i < foodTruckArr.length; i++) {
+			if (foodTruckArr[i] == null) {
+				continue;
+			}
+			sum += foodTruckArr[i].getRating();
+			j++;
+		}
+		average = sum / j;
+		System.out.println("------------------------");
+		System.out.println("Average rating is: " + average);
+		System.out.println("------------------------");
+	}
+	
+	public void getHighestRatedTruck(FoodTruck[] foodTruckArr) {
+		double highestRated = 0;
+		int j = 0;
+		for(int i = 0; i < foodTruckArr.length; i++) {
+			if (foodTruckArr[i] == null) {
+				continue;
+			} else if (foodTruckArr[i].getRating() > highestRated) {
+				highestRated = foodTruckArr[i].getRating();
+				j = i;
+			}
+		}
+		System.out.println("------------------------");
+		System.out.println(foodTruckArr[j].toString());
+		System.out.println("------------------------");
+	}
+	
 }
